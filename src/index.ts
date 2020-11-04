2df5f8f7c36ce4348098796a734afe649ed5667b
Leon Koole <leon@koole.io>
2020-11-05 00:43:27 +0100

    Intro audio

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import "./cursor";
 import "./footer";
+import "./intro";
 
 import VideoToFrames, { VideoToFramesMethod } from "./toframes";
+
+export const E = (id: string): HTMLElement | null => document.getElementById(id);
 
 interface Project {
   id: HTMLElement | null;
@@ -14,7 +17,6 @@ interface Project {
   imageList: ImageData[];
 }
 
-const E = (id: string): HTMLElement | null => document.getElementById(id);
 
 const projects: Project[] = [
   {
@@ -122,98 +124,3 @@ function scrollPlay(): void {
 }
 
 window.requestAnimationFrame(scrollPlay);
-
-function docReady(fn) {
-  // see if DOM is already available
-  if (
-    document.readyState === "complete" ||
-    document.readyState === "interactive"
-  ) {
-    // call on next available tick
-    setTimeout(fn, 1);
-  } else {
-    document.addEventListener("DOMContentLoaded", fn);
-  }
-}
-
-docReady(() => {
-  console.warn("Loaded!");
-  const video = E("header-video");
-  const T0 = E("header-text-0");
-  const T1 = E("header-text-1");
-  const T2 = E("header-text-2");
-  if (video !== null) {
-    setTimeout(() => {
-      video.style.opacity = "1";
-    }, 800);
-    setTimeout(() => {
-      video.style.opacity = "0";
-    }, 900);
-    setTimeout(() => {
-      video.style.opacity = "1";
-    }, 1500);
-    setTimeout(() => {
-      video.style.opacity = "0";
-    }, 1600);
-    setTimeout(() => {
-      video.style.opacity = "1";
-    }, 2300);
-    setTimeout(() => {
-      video.style.opacity = "0";
-    }, 2400);
-    setTimeout(() => {
-      video.style.opacity = "1";
-    }, 3500);
-  }
-  if (T0 !== null) {
-    setTimeout(() => {
-      T0.classList.add("active");
-    }, 900);
-    setTimeout(() => {
-      T0.classList.remove("active");
-    }, 1000);
-    setTimeout(() => {
-      T0.classList.add("active");
-    }, 2000);
-    setTimeout(() => {
-      T0.classList.remove("active");
-    }, 2100);
-    setTimeout(() => {
-      T0.classList.add("active");
-    }, 3700);
-  }
-  if (T1 !== null) {
-    setTimeout(() => {
-      T1.classList.add("active");
-    }, 1200);
-    setTimeout(() => {
-      T1.classList.remove("active");
-    }, 1300);
-    setTimeout(() => {
-      T1.classList.add("active");
-    }, 1800);
-    setTimeout(() => {
-      T1.classList.remove("active");
-    }, 1900);
-    setTimeout(() => {
-      T1.classList.add("active");
-    }, 3900);
-  }
-  if (T2 !== null) {
-    setTimeout(() => {
-      T2.classList.add("active");
-    }, 1000);
-    setTimeout(() => {
-      T2.classList.remove("active");
-    }, 1150);
-    setTimeout(() => {
-      T2.classList.add("active");
-    }, 2100);
-    setTimeout(() => {
-      T2.classList.remove("active");
-    }, 2200);
-    setTimeout(() => {
-      T2.classList.add("active");
-    }, 4000);
-  }
-});
---
+++ src/intro.ts
@@ -0,0 +1,58 @@
+import { E } from ".";
+
+// Fixes delayed audio playback on Safari
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
+
+function docReady(fn) {
+  // see if DOM is already available
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    // call on next available tick
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+docReady(() => {
+  window.scrollTo(0, 0);
+  setTimeout(() => {
+    document.body.style.overflow = "hidden";
+  }, 10);
+  const startButton = E("light-button") as HTMLButtonElement;
+  const audio = E("light-audio") as HTMLAudioElement;
+  const video = E("header-video");
+  const T0 = E("header-text-0");
+  const T1 = E("header-text-1");
+  const T2 = E("header-text-2");
+  audio.addEventListener("play", () => {
+    if (video !== null) {
+      setTimeout(() => {
+        video.style.opacity = "1";
+      }, 1319);
+    }
+    if (T0 !== null) {
+      setTimeout(() => {
+        T0.classList.add("active");
+      }, 2994);
+    }
+    if (T1 !== null) {
+      setTimeout(() => {
+        T1.classList.add("active");
+      }, 3597);
+    }
+    if (T2 !== null) {
+      setTimeout(() => {
+        T2.classList.add("active");
+        document.body.style.overflow = "unset";
+      }, 3859);
+    }
+  });
+  startButton.addEventListener("click", () => {
+    audio.play();
+    startButton.style.display = "none";
+  });
+});
