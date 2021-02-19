db20b77e4bb1662eab7dc956b27d0dde13941dfd
Leon Koole <leon@koole.io>
2021-02-19 19:30:06 +0100

    Remove unused controls import

--- src/projects/Revision.ts
+++ src/projects/Revision.ts
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { MeshLine, MeshLineMaterial } from "three.meshline";
 import { readyCallback } from "../header";
 
