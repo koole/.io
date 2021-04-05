54ea5d0f9e0ab5b265c03c5220f58958717874bd
Leon Koole <leon@koole.io>
2021-04-05 23:36:05 +0200

    Fix logo on mobile

--- src/logo.ts
+++ src/logo.ts
@@ -3,7 +3,11 @@ import { docReady, E } from "./utils";
 docReady(() => {
   const logo = E("logo") as HTMLButtonElement;
   const audio = E("logo-audio") as HTMLAudioElement;
-  logo.addEventListener("click", () => {
+  logo.addEventListener("mousedown", () => {
+    audio.currentTime = 0;
+    audio.play();
+  });
+  logo.addEventListener("touchdown", () => {
     audio.currentTime = 0;
     audio.play();
   });
