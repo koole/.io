b3a5d22e91834c8a6a87f4668c7d0fd15e5e16ef
Leon Koole <leon@koole.io>
2021-04-11 14:18:53 +0200

    Small fixes

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ import "./logo";
 import "./debug";
 import "./party";
 
+console.info(
+  "%c            \n  DE        \n  KOOLE     \n  CENTRALE  \n            ",
+  'font-weight: bold; font-size: 50px;background:black;color:#fff; line-height: 1;font-family:Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;'
+);
 // import Stats from "stats.js";
 // const stats = new Stats();
 // stats.dom.className = "statsjs";
